bbd76152bc932704c9861741d709c6a07898487a
Robert Melendez <melendez@robertdev.net>
2024-03-19 09:16:03 -0400

    pushing

--- app/checkout.tsx
+++ app/checkout.tsx
@@ -1,9 +1,7 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { StyleSheet, Text, View } from "react-native";
 
 const Checkout = () => {
-  const insets = useSafeAreaInsets();
   return (
     <View
       style={{
